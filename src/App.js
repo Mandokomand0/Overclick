e21df6c8b9c40fff26aea39d3fa2b9081ff54df7
Fu Ck U <d.foxultra@gmail.com>
2018-01-05 17:56:52 -0600

    servers and routes added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
+import ReactModal from "react-modal";
+import ReactDOM from "react-dom";
+// import jQuery from "jquery";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Game from "./components/Game";
 
-const App = () => <Game />;
+const App = () =>
+  <Router>
+    <div>
+        <Route exact path="/" component={Game} />
+    </div>
+  </Router>;
 
 export default App;
